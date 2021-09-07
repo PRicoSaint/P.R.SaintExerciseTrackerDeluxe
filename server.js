4bc3426fe1ffb20383a516a1abc7fda20a050794
Esteban Santos <santosesteban09@gmail.com>
2021-09-06 20:52:00 -0500

    Added routes to reach stats and exercise pages.

--- server.js
+++ server.js
@@ -25,6 +25,14 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "./public/index.html"));
 });
 
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/exercise.html"));
+  });
+
+app.get("/stats", (req, res) => {
+res.sendFile(path.join(__dirname + "/public/stats.html"));
+});
+
 // app.post("/submit", (req, res) => {
 //   console.log(req.body);
 
